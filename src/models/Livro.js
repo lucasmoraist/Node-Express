b8ba129d503e21da3f2b13478e13b0b7d85a5c91
lucasmoraisnt <rm93267@fiap.com.br>
2023-10-18 13:49:28 -0300

    Corrigindo

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -5,7 +5,7 @@ const livroSchema = new mongoose.Schema(
         id: {type: String},
         titulo: {type: String, required: true},
         autor: {type: String, required: true},
-        editor: {type: String, required: true},
+        editora: {type: String, required: true},
         numeroPaginas: {type: Number}
     }
 );
