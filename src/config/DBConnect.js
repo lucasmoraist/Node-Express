f2e55839f7e999f03c8daff4b9e488851c347ed6
Lucas de Morais <112484844+lucasmoraist@users.noreply.github.com>
2023-10-18 16:32:26 -0300

    Update DBConnect.js aberto

--- src/config/DBConnect.js
+++ src/config/DBConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://root:123@lucas.sr6tzmd.mongodb.net/alura-node");
+mongoose.connect("mongodb+srv://root:1234@cluster0.kyhc6m0.mongodb.net/aberto");
 
 let db = mongoose.connection;
 
