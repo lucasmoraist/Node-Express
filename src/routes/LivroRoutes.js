1dce1ff63798ef09714d43471d75beb82f61c186
lucasmoraisnt <rm93267@fiap.com.br>
2023-10-18 16:09:07 -0300

    Consultas com query

--- src/routes/LivroRoutes.js
+++ src/routes/LivroRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router
     .get("/livros", LivroController.listarLivros)
+    .get("/livros/busca", LivroController.listaPorEditora)
     .get("/livros/:id", LivroController.listaLivro)
     .post("/livros", LivroController.cadastrarLivro)
     .put("/livros/:id", LivroController.atualizaLivro)
