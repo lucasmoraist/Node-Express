08a7a006cfc7b7a57f067c6858f3eafdd659c23f
lucasmoraisnt <rm93267@fiap.com.br>
2023-10-23 16:36:39 -0300

    Criando crud

---
+++ src/controllers/PessoaController.js
@@ -0,0 +1,42 @@
+const bd = require('../models')
+
+class PessoaController {
+
+    static async findAll(req, res){
+        try{
+            const pessoas = await bd.Pessoas.findAll();
+            return res.status(200).json(pessoas)
+        }catch(error){
+            return res.status(500).json(error.message)
+        }
+        
+    }
+
+    static async findById(req, res){
+        const { id } = req.params
+
+        try{
+            const pessoa = await bd.Pessoas.findOne( { 
+                where: { id: Number(id) 
+               } 
+        })
+        } catch (err) {
+
+        }
+    }
+
+    static async create(req, res){
+
+    }
+
+    static async update(req, res){
+
+    }
+
+    static delete(req, res){
+
+    }
+
+}
+
+module.exports = PessoaController;
