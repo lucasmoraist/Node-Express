b74a882a3d1ef73ada1f3cec9d12026a0217150b
lucasmoraisnt <rm93267@fiap.com.br>
2023-10-24 08:52:39 -0300

    Requisições Update e Delete

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -9,7 +9,6 @@ class PessoaController {
         } catch (error) {
             return res.status(500).json(error.message)
         }
-
     }
 
     static async findById(req, res) {
@@ -19,7 +18,7 @@ class PessoaController {
             const pessoa = await bd.Pessoas.findOne({
                 where: {
                     id: Number(id)
-                }
+                }// Posso utilizar do where também detro do findAll
             })
             return res.status(200).json(pessoa)
         } catch (error) {
@@ -37,11 +36,30 @@ class PessoaController {
     }
 
     static async update(req, res) {
+        const { id } = req.params
+        const infos = req.body
 
+        try{
+            await bd.Pessoas.update(infos, { where: { id: Number(id) } } )
+            const pessoaAtualizada = await bd.Pessoas.findOne( { where: { id: Number(id) } } )
+            return res.status(200).json(pessoaAtualizada)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
     }
 
-    static delete(req, res) {
+    static async delete(req, res) {
+        const { id } = req.params
 
+        try{
+            await bd.Pessoas.destroy({
+                where: { id: Number(id) }
+            })
+
+            return res.status(200).send('Excluido com sucesso')
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
     }
 
 }
--- src/routes/pessoaRoute.js
+++ src/routes/pessoaRoute.js
@@ -7,5 +7,7 @@ router
     .get('/pessoas', PessoaController.findAll)
     .get('/pessoas/:id', PessoaController.findById)
     .post('/pessoas', PessoaController.create)
+    .put('/pessoas/:id', PessoaController.update)
+    .delete('/pessoas/:id', PessoaController.delete)
 
 module.exports = router
