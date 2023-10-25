4adbe2d22371d43062a817586d4d11b7a453482d
lucasmoraisnt <rm93267@fiap.com.br>
2023-10-25 13:37:07 -0300

    Removendo as requisições das matriculas

--- src/routes/pessoaRoute.js
+++ src/routes/pessoaRoute.js
@@ -7,8 +7,11 @@ router
     .get('/pessoas', PessoaController.findAll)
     .get('/pessoas/:id', PessoaController.findById)
     .get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.findByIdMatricula)
+    .post('/pessoas/:estudanteId/matricula', PessoaController.createMatricula)
     .post('/pessoas', PessoaController.create)
     .put('/pessoas/:id', PessoaController.update)
+    .put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.updateMatricula)
     .delete('/pessoas/:id', PessoaController.delete)
+    .delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.deleteMatricula)
 
 module.exports = router
