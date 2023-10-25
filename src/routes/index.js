6ba37e6efb541a8d3e9c81f27330749659d9ba3d
lucasmoraisnt <rm93267@fiap.com.br>
2023-10-25 13:37:38 -0300

    Incluindo rota de matricula no app

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,14 @@ const bodyParser = require('body-parser')
 const pessoas = require('./pessoaRoute.js')
 const niveis = require('./nivelRoute.js')
 const turmas = require('./turmaRoute.js')
+const matriculas = require('./matriculaRoute.js')
 
 module.exports = app => {
  app.use(
    bodyParser.json(),
    pessoas,
    niveis,
-   turmas
+   turmas,
+   matriculas
    )
  }
